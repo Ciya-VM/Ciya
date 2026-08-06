91c3fff62314018261f9f9f7db534615d4428e2c
Nguyen Phuoc Thanh Lam <phuocthanhlamnguyen@gmail.com>
2026-08-06 16:11:42 +0700

    Update repl.c
    Remove comments, as I mainly work on REPL

--- src/utils/repl.c
+++ src/utils/repl.c
@@ -55,21 +55,21 @@ void REPL(char* argv[]) {
 
   printf("Note: Type '.help' to view the current commands\n");
   while (1) {
-    // An repl uses an "infinite" number of chars in input which means that NO LIMIT 
-    // to achieve this, I'll make a custom version of getline(). 
+    // An repl uses an "infinite" number of chars in input which means that NO LIMIT
+    // to achieve this, I'll make a custom version of getline().
     printf(">>> "); // print the starting thing
     char* input = getLine(); // in here, we use a pointer to make it dynamically expandable
     Lexer lexer;
     if (strcmp(input, ".exit") == 0){
       printf("Exiting...\n");
       free(input);
-      input = NULL; //Don't forget the dangling pointers =)
+      input = NULL;
       return;
-    } else if (strcmp(input, ".linktosource") == 0){ //I change link to source because the word code is broad, but source mean in where is it from
+    } else if (strcmp(input, ".linktosource") == 0){
       printf("link: https://github.com/johnryzon123/Ciya.git\n");
 
       #if defined(__linux__)
-      system("xdg-open https://github.com/johnryzon123/Ciya.git"); 
+      system("xdg-open https://github.com/johnryzon123/Ciya.git");
       #elif defined(__APPLE__)
       system("open https://github.com/johnryzon123/Ciya.git");
       #elif defined(_WIN32) || defined(WIN64_)
@@ -77,7 +77,7 @@ void REPL(char* argv[]) {
       #endif
       free(input);
       input = NULL;
-    } else if (strcmp(input, ".freemem") == 0){ //Add it to free memory (idk why)
+    } else if (strcmp(input, ".freemem") == 0){
       free(input);
       input = NULL;
       printf("Memory free!\n");
@@ -104,7 +104,7 @@ void REPL(char* argv[]) {
       run(lexer.tokens.tokens);
       free(lexer.tokens.tokens);
     }
-    free(input); // make sure we free the pointer, we don't want any memory leaks
+    free(input);
     input = NULL;
   }
 }
